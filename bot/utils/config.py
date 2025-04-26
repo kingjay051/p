106642cdc559d849e5e862e3e8724ececfdb0cb5
Azello-XD <jonathanlenathea@gmail.com>
2025-04-27 02:20:37 +0700

    Update config.py

--- bot/utils/config.py
+++ bot/utils/config.py
@@ -6,7 +6,7 @@ class Config:
     API_HASH: str = os.environ.get("API_HASH", "93acdbdc4b23c0398bd2042d5ee3865e") #GANTI "b184" DENGAN API_HASH , JANGAN HAPUS TANDA (" ") NYA !!!
     BOT_TOKEN: str = os.environ.get("BOT_TOKEN", "7694374653:AAG4CwdnZ4h6z9CnNe2JQfU58Aea55Bnjkc") #GANTI "7u9jl" DENGAN TOKEN BOT , JANGAN HAPUS TANDA (" ") NYA !!!
     OWNER_ID = int(os.environ.get("OWNER_ID", 6433540918)) #GANTI "1234" DENGAN ID PEMILIK BOT
-    MONGODB_URI: str = os.environ.get("MONGODB_URI", "mongodb+srv://jonathanlenathea:memek@kontollllllllll.wcujhle.mongodb.net/?retryWrites=true&w=majority&appName=Kontollllllllll") #GANTI "mongodb://root:passwd@mongo" DENGAN URI MONGO , JANGAN HAPUS TANDA (" ") NYA !!!
+    MONGODB_URI: str = os.environ.get("MONGODB_URI", "mongodb://iyaaaaaa:anjingggg@iyaaanjing-shard-00-00.uzkxmrz.mongodb.net:27017,iyaaanjing-shard-00-01.uzkxmrz.mongodb.net:27017,iyaaanjing-shard-00-02.uzkxmrz.mongodb.net:27017/?ssl=true&replicaSet=atlas-uzkxmrz-shard-0&authSource=admin&retryWrites=true&w=majority&appName=iyaaanjing") #GANTI "mongodb://root:passwd@mongo" DENGAN URI MONGO , JANGAN HAPUS TANDA (" ") NYA !!!
     DATABASE_CHAT_ID = int(os.environ.get("DATABASE_CHAT_ID", -1002278948500)) #GANTI DENGAN DATABASE CHANNEL
 
 
