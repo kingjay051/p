244178d6a4fa51390636bcc89ca57347bd2c1b0c
Azello-XD <jonathanlenathea@gmail.com>
2025-04-27 04:09:56 +0700

    Update config.py

--- bot/utils/config.py
+++ bot/utils/config.py
@@ -7,7 +7,7 @@ class Config:
     BOT_TOKEN: str = os.environ.get("BOT_TOKEN", "7749393831:AAHkuC3iVnC0k5kmW9hdUH-BpiGCyjmWkvs") #GANTI "7u9jl" DENGAN TOKEN BOT , JANGAN HAPUS TANDA (" ") NYA !!!
     OWNER_ID = int(os.environ.get("OWNER_ID", 1927018403)) #GANTI "1234" DENGAN ID PEMILIK BOT
     MONGODB_URI: str = os.environ.get("MONGODB_URI", "mongodb+srv://wtfbruh:KontolXD#123@fsub.brzgete.mongodb.net/?retryWrites=true&w=majority&appName=fsub") #GANTI "mongodb://root:passwd@mongo" DENGAN URI MONGO , JANGAN HAPUS TANDA (" ") NYA !!!
-    DATABASE_CHAT_ID = int(os.environ.get("DATABASE_CHAT_ID", -1002517617021)) #GANTI DENGAN DATABASE CHANNEL
+    DATABASE_CHAT_ID = int(os.environ.get("DATABASE_CHAT_ID", -1002516449942)) #GANTI DENGAN DATABASE CHANNEL
 
 
 config: "Config" = Config()
